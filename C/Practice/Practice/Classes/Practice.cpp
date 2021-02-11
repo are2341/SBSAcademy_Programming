90bacea5ac4d7e622438ea85d206428087f3815f
dante <are2341@nate.com>
2021-02-12 03:27:51 +0900

    no message

---
+++ C/Practice/Practice/Classes/Practice.cpp
@@ -0,0 +1,7 @@
+#include "Practice_01/Practice_01.h"
+
+//! 메인 함수
+int main(const int argc, const char **args) {
+	Practice_01(argc, args);
+	return 0;
+}
---
+++ C/Practice/Practice/Classes/Practice_01/Practice_01.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../Global/Define/KGDefine.h"
+
+//! Practice 1
+void Practice_01(const int argc, const char **args);
